84aa325b5dfcaffc04160319bf0ddf99a659793b
Nick Galante <nicholasgalante1997@gmail.com>
2024-05-29 00:27:09 -0600

    feat: Actix web static file server

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use actix_files as fs;
+use actix_web::{web, App, HttpServer, Responder};
+
+mod service_info;
+use service_info::ServiceInfo;
+
+async fn service_info() -> impl Responder {
+    ServiceInfo::new()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/x/app/info", web::get().to(service_info))
+            .service(
+                fs::Files::new("/", "./dotafts-www")
+                .show_files_listing()
+                .use_last_modified(true)
+                .use_etag(true)
+                .prefer_utf8(true)
+                .index_file("./dotafts-www/index.html")
+            )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
---
+++ src/service_info.rs
@@ -0,0 +1,51 @@
+use std::env;
+
+use actix_web::{
+    body::BoxBody, http::header::ContentType, HttpRequest, HttpResponse, Responder,
+};
+use dotenv::dotenv;
+use serde_json;
+use serde::Serialize;
+use serde::Deserialize;
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct ServiceInfo {
+    application_name: String,
+    application_version: String,
+    commit_hash: String,
+    xsi_signature: String
+}
+
+impl ServiceInfo {
+    pub fn new() -> Self {
+        dotenv().ok();
+        let missing_env_var_text = String::from("Unknown");
+        Self {
+            application_name: env::var("X_APPLICATION_NAME").unwrap_or(missing_env_var_text.clone()),
+            application_version: env::var("X_APPLICATION_VERSION").unwrap_or(missing_env_var_text.clone()),
+            commit_hash: String::from(""),
+            xsi_signature: String::from("")
+        }
+    }
+}
+
+// Responder
+impl Responder for ServiceInfo {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        // Create response and set content type
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
+}
+
+// SECTION 
+// see documentation:
+// - https://actix.rs/docs/handlers 
+// - https://docs.rs/crate/serde_json/latest
+// - https://crates.io/crates/serde
+// !SECTION
