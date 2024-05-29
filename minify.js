6a15ec350a181dc6942bee37a592ede5ffb97946
Nick Galante <nicholasgalante1997@gmail.com>
2024-05-28 21:33:51 -0600

    feat: Finishes minifier, uses minified JS in html files

--- minify.js
+++ minify.js
@@ -1,12 +1,11 @@
-import { minify } from 'terser';
-import { glob } from 'glob';
-
-import { Option, Log, Timer } from 'sleepydogs';
-
-import path from 'node:path';
-import { readFile } from 'node:fs/promises';
+import { mkdir, readFile, writeFile } from 'node:fs/promises';
 import { existsSync, mkdirSync, rmSync } from 'node:fs';
-import { Attempt } from 'sleepydogs';
+import os from 'node:os';
+import path from 'node:path';
+
+import { glob } from 'glob';
+import { Attempt, Option, Log, Timer } from 'sleepydogs';
+import { minify } from 'terser';
 
 const OUT = path.resolve(process.cwd(), 'dist');
 
@@ -16,6 +15,66 @@ const daml = Log.factory({
   version: '1.0.0'
 });
 
+/**
+ * Asynchronously writes the param `code` to the provided `minPath`
+ * @param {string} minPath 
+ * @param {string} code 
+ */
+async function writeMinJsFile(minPath, code) {
+  const writeAttempt = new Attempt({
+    callback: async () => await writeFile(minPath, code, { encoding: "utf8" }),
+    onError: exitOnError,
+    retries: 5
+  });
+  await writeAttempt.run();
+}
+
+/**
+ * Creates a file's root (immediate parent) directory using the provided sourcePath.
+ * @param {string} sourcePath 
+ */
+async function createParentDirectory(sourcePath) {
+  const delimiter = getOS() === 'win32' ? '\\' : '/';
+  const dirPath = sourcePath.split(delimiter).slice(0, -1).join(delimiter);
+  await mkdir(dirPath, { recursive: true });
+}
+
+/**
+ * Creates a minPath from a sourcePath
+ * @param {string} sourcePath
+ * @param {boolean} sourceMap 
+ */
+function getMinPath(sourcePath, sourceMap = false) {
+  const delimiter = getOS() === 'win32' ? '\\' : '/';
+  const splitPath = sourcePath.split(delimiter);
+  const srcIndex = splitPath.indexOf("src");
+
+  if (srcIndex === -1) {
+    daml.error("FileNotSrcException");
+    daml.error(path);
+    exitOnError();
+  }
+
+  const localPathArr = splitPath.slice(srcIndex + 1);
+  const filename = localPathArr[localPathArr.length - 1];
+
+  const minFileName = filename.repeat(1).replace('.js', sourceMap ? '.map.json' : '.js');
+  localPathArr[localPathArr.length - 1] = minFileName;
+
+  return path.resolve(OUT, ...localPathArr);
+}
+
+/**
+ * @returns {NodeJS.Platform}
+ */
+function getOS() {
+  return os.platform();
+}
+
+/**
+ * Uses `glob` to get all js files in src
+ * @returns string[]
+ */
 async function getAllJsFilePaths() {
   const globOption = new Option(
     async () => await glob('src/**/*.js', { ignore: /node_modules|([\d\w])*\.min\.js/g }),
@@ -77,13 +136,16 @@ function getPreamble(source) {
     hour12: false,
     timeZone: 'America/Denver'
   };
-  return `********************************************************************************
-            source-path: "${source}" 
-            generated on "${new Intl.DateTimeFormat('en-US', intlOptions).format(new Date())}"
-        ********************************************************************************\n`;
+  return (
+    `/* ********************************************************************************
+  * source-path: "${source}" 
+  * source-map: "${getMinPath(source, true)}"
+  * generated on "${new Intl.DateTimeFormat('en-US', intlOptions).format(new Date())}"
+  ******************************************************************************** */\n`
+  );
 }
 
-function exitOnError(e) {
+function exitOnError(e = null) {
   daml.error(e);
   cleanDist();
   process.exit(1);
@@ -120,9 +182,20 @@ async function build() {
   let minCodeArr = [];
 
   for (let jsObj of sourceCode) {
-    minCodeArr.push(await minifyJs(jsObj));
+    const minJs = await minifyJs(jsObj);
+    minCodeArr.push({ source: jsObj.source, ...minJs });
   }
 
+  for (const { source, code, map } of minCodeArr) {
+    const minCodePath = getMinPath(source);
+    const minSourceMapPath = getMinPath(source, true);
+    await createParentDirectory(minCodePath);
+    await Promise.all([writeMinJsFile(minCodePath, code), writeMinJsFile(minSourceMapPath, map)]);
+  }
+
+  timer.stop();
+  daml.log('Done.')
+  daml.log(`Run took ${timer.elapsed()}ms`);
 }
 
 await build();
